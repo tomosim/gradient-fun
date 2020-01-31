d194f7b322e913cec88a260a9cb551b7565f5041
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2020-01-31 17:13:15 +0000

    create background

---
+++ src/Components/Styled/Background.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Background = styled.div`
+  min-height: 100vh;
+  background-color: black;
+  background-image: linear-gradient(
+    ${props =>
+      `rgb(${props.colourOne[0]},${props.colourOne[1]},${props.colourOne[2]})`},
+    ${props =>
+      `rgb(${props.colourTwo[0]},${props.colourTwo[1]},${props.colourTwo[2]})`}
+  );
+  background-position: 10px 10px, 100% 0;
+  background-repeat: no-repeat;
+  -webkit-background-size: calc(100vw - 20px) calc(100vh - 20px);
+  -moz-background-size: calc(100vw - 20px) calc(100vh - 20px);
+  background-size: calc(100vw - 20px) calc(100vh - 20px);
+`;
+
+export default Background;
